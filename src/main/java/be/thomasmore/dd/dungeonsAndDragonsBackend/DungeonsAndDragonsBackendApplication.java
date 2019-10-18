2dfcb7c8435db87183785643bb324b98dc28ecc1
DinVanwezemael <37238893+DinVanwezemael@users.noreply.github.com>
2019-10-18 11:18:29 +0200

    eurekaserver toegevoegd

--- src/main/java/be/thomasmore/dd/dungeonsAndDragonsBackend/DungeonsAndDragonsBackendApplication.java
+++ src/main/java/be/thomasmore/dd/dungeonsAndDragonsBackend/DungeonsAndDragonsBackendApplication.java
@@ -2,8 +2,10 @@ package be.thomasmore.dd.dungeonsAndDragonsBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DungeonsAndDragonsBackendApplication {
 
 	public static void main(String[] args) {
