ab63ae17cfc3b40cded21cdb4aca74e14c1a9455
DinVanwezemael <37238893+DinVanwezemael@users.noreply.github.com>
2019-10-18 10:26:59 +0200

    first commit

---
+++ src/main/java/be/thomasmore/dd/dungeonsAndDragonsBackend/DungeonsAndDragonsBackendApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.dd.dungeonsAndDragonsBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonsAndDragonsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonsAndDragonsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/thomasmore/dd/dungeonsAndDragonsBackend/DungeonsAndDragonsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.dd.dungeonsAndDragonsBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonsAndDragonsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
