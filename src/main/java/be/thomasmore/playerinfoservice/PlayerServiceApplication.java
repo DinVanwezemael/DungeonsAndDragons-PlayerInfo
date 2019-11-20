796818bf21c5bebdf2a76e2001657b9fb3b89cdb
mnmpower <mnmmaarten@hotmail.com>
2019-11-20 13:06:43 +0100

    workign player service examples

--- src/main/java/be/thomasmore/playerinfoservice/PlayerServiceApplication.java
+++ src/main/java/be/thomasmore/playerinfoservice/PlayerServiceApplication.java
@@ -1,19 +1,41 @@
 package be.thomasmore.playerinfoservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 @SpringBootApplication
+@RestController
 public class PlayerServiceApplication {
-//	private static Logger log = LoggerFactory.getLogger(PlayerServiceApplication.class);
+
+	private static Logger log = LoggerFactory.getLogger(PlayerServiceApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlayerServiceApplication.class, args);
 	}
 
-//	@RequestMapping(value = "/")
-//	public String home() {
-//		log.info("Access /(niets dus hi)");
-//		return "Hi!PLAYER SERVICE";
-//	}
+	@RequestMapping(value = "/")
+	public String home() {
+		log.info("Access /(niets dus hi)");
+		return "Hi!ssss8001";
+	}
+
+	@RequestMapping(value = "/greeting")
+	public String greet() {
+		log.info("Access /greeting");
+
+		List<String> greetings = Arrays.asList("Hi there8001", "Greetings8001", "Salutations8001");
+		Random rand = new Random();
+
+		int randomNum = rand.nextInt(greetings.size());
+		return greetings.get(randomNum);
+	}
+
 }
--- src/main/java/be/thomasmore/playerinfoservice/Repository/PlayerRepository.java
+++ src/main/java/be/thomasmore/playerinfoservice/Repository/PlayerRepository.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin
 public interface PlayerRepository extends JpaRepository<Player, Integer> {
-    Player findPlayerById(@Param("id") int id);
+//    Player findPlayerById(@Param("id") int id);
 }
